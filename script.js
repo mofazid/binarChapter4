7a65d92256b258603da1dde842af9be96368c62c
mofazid <mofazid@gmail.com>
2022-07-25 23:51:20 +0700

    final result

--- script.js
+++ script.js
@@ -28,57 +28,53 @@ const insertImgsCOM = () => {
 insertImgsCOM()
  
 
-$("#image-player").click(function(event){
+let play = () => {
+  $("#image-player").one('click',function(event){
 
-  pilihanUser = event.target.className;
-  pilihanCom = generateComputerOption()
+    pilihanUser = event.target.className;
+    pilihanCom = generateComputerOption()
 
-  if (pilihanUser == pilihanCom) {
-    $(".vs").replaceWith(`<img src= "img/DRAW.png" class="vs">`)
-    $(".vs").css({"background-color": "green"});
-  }
-  
-  if (
-    pilihanUser == "batu" && pilihanCom == "gunting" || 
-    pilihanUser == "gunting" && pilihanCom == "kertas" || 
-    pilihanUser == "kertas" && pilihanCom == "batu"
-  ) {
-    $(".vs").replaceWith(`<img src= "img/player1win.png" class="vs">`)
-    $(".vs").css({"background-color": "green"});
-  }
+    if (pilihanUser == pilihanCom) {
+      $(".vs").replaceWith(`<h1 class="result">DRAW</h1>`)
+    }
+    
+    if (
+      pilihanUser == "batu" && pilihanCom == "gunting" || 
+      pilihanUser == "gunting" && pilihanCom == "kertas" || 
+      pilihanUser == "kertas" && pilihanCom == "batu"
+    ) {
+      $(".vs").replaceWith(`<h1 class="result">PLAYER 1 WIN</h1>`)
+    }
 
-  if (
-    pilihanUser == "batu" && pilihanCom == "kertas" || 
-    pilihanUser == "gunting" && pilihanCom == "batu" || 
-    pilihanUser == "kertas" && pilihanCom == "gunting"
-  ) {
-    $(".vs").replaceWith(`<img src= "img/COMWIN.png" class="vs">`)
-    $(".vs").css({"background-color": "green"});
-  }
+    if (
+      pilihanUser == "batu" && pilihanCom == "kertas" || 
+      pilihanUser == "gunting" && pilihanCom == "batu" || 
+      pilihanUser == "kertas" && pilihanCom == "gunting"
+    ) {
+      $(".vs").replaceWith(`<h1 class="result">COM <br> WIN</h1>`)
+    }
 
-  $(event.target).css({
-    "background-color": "#C4C4C4",
-    "border-radius": "30px"
-  });  
+    $(event.target).css({
+      "background-color": "#C4C4C4",
+      "border-radius": "30px"
+    }); 
+  })
+}
+play()
 
-  console.log(pilihanCom)
-
-  $(event).off()
-})
 
 
 const generateComputerOption = () => {
 
   let number = Math.floor(Math.random() * 3) 
   pilihanCom = list[number]
-  const collection = document.getElementById("image-com").children;
-  collection[number].classList.add("picked")
+  document.getElementById("image-com").children[number].classList.add("picked");
   return pilihanCom
 }
 
 
 $(".refresh").on("click", function(event){
-  let confirmResult = confirm("retry?")
+  let confirmResult = confirm("Retry?")
   if (confirmResult){
     location.reload()
   }
